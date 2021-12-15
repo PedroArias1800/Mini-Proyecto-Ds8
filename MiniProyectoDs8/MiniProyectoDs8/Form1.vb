91d015aa57fe87ebde589bdff7a5284f8e76a952
Aurelio Morales <aurelio.morales@utp.ac.pa>
2021-12-15 14:06:09 -0500

    LookupCity
    Funcionalidad basica de la pestaña Lookupcity

--- MiniProyectoDs8/MiniProyectoDs8/Form1.vb
+++ MiniProyectoDs8/MiniProyectoDs8/Form1.vb
@@ -10,11 +10,15 @@
         SumarEnteros.Show()
     End Sub
 
-
-
     Private Sub ObtenerConjuntoDeDatosToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ObtenerConjuntoDeDatosToolStripMenuItem.Click
         GetListByNamevb.MdiParent = Me
         GetListByNamevb.WindowState = FormWindowState.Maximized
         GetListByNamevb.Show()
+    End Sub
+
+    Private Sub BuscarCiudadToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles BuscarCiudadToolStripMenuItem.Click
+        __LookupCity.MdiParent = Me
+        __LookupCity.WindowState = FormWindowState.Maximized
+        __LookupCity.Show()
     End Sub
 End Class
--- MiniProyectoDs8/MiniProyectoDs8/LookupCity.vb
+++ MiniProyectoDs8/MiniProyectoDs8/LookupCity.vb
@@ -1,3 +1,13 @@
-﻿Public Class __LookupCity
+﻿Imports Newtonsoft.Json
 
+Public Class __LookupCity
+    Dim Servicios As New WebServiceSoap.SOAPDemoSoapClient
+    Private Sub btnBuscar_Click(sender As Object, e As EventArgs) Handles btnBuscar.Click
+        Dim zip As String
+        Dim data() As String
+        zip = txtZipCode.Text
+        Dim JsonData As String = JsonConvert.SerializeObject(Servicios.LookupCity(zip))
+        'MsgBox(JsonData)
+        txtInfo.Text = JsonData
+    End Sub
 End Class
