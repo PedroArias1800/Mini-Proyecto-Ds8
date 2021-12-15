f9798d9ace87d2637e7311df12d0e5c01c43eadc
PedroArias1800 <86125486+PedroArias1800@users.noreply.github.com>
2021-12-15 16:31:14 -0500

    Revert "Merge branch 'main' of https://github.com/PedroArias1800/Mini-Proyecto-Ds8"
    This reverts commit 602e0c63cc4fc558be87c31a4e657bf8c8644f82, reversing
    changes made to a17435cfddca7205c489b7ef5530bdd53b1fe2d7.

--- MiniProyectoDs8/MiniProyectoDs8/Form1.vb
+++ MiniProyectoDs8/MiniProyectoDs8/Form1.vb
@@ -36,8 +36,4 @@
         __LookupCity.WindowState = FormWindowState.Maximized
         __LookupCity.Show()
     End Sub
-
-    Private Sub MiniProyecto_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Me.WindowState = FormWindowState.Maximized
-    End Sub
 End Class
