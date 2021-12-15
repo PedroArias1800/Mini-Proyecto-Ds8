a8a464e86c165995581beb77b287d840f028eedc
PedroArias1800 <86125486+PedroArias1800@users.noreply.github.com>
2021-12-15 15:36:54 -0500

    Hay listos 3 de 6
    Los de sumar numero, buscar persona y obtener data de por un nombre ya funcionan bien en todo

--- MiniProyectoDs8/MiniProyectoDs8/SumarEnteros.vb
+++ MiniProyectoDs8/MiniProyectoDs8/SumarEnteros.vb
@@ -7,13 +7,37 @@ Public Class SumarEnteros
         Dim datas() As String
         num1 = Val(TextBox1.Text)
         num2 = Val(TextBox2.Text)
-        TextBox3.Text = Servicios.AddInteger(num1, num2)
-        Dim JsonData As String = JsonConvert.SerializeObject(Servicios.FindPerson(num1))
-        datas = Split(JsonData, "-")
-        MsgBox(JsonData)
+
+        If TextBox1.Text = "" Or TextBox2.Text = "" Then
+            MsgBox("Primero inserte 2 valores para sumarlos...", vbYes, "Ha ocurrido un error...")
+
+        Else
+            TextBox3.Text = Servicios.AddInteger(num1, num2)
+            Dim JsonData As String = JsonConvert.SerializeObject(Servicios.FindPerson(num1))
+            datas = Split(JsonData, "-")
+
+        End If
+
+    End Sub
+
+    Private Sub TextBox1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox1.KeyPress
+        If Asc(e.KeyChar) <> 8 Then
+            If Asc(e.KeyChar) < 48 Or Asc(e.KeyChar) > 57 Then
+                e.Handled = True
+            End If
+        End If
+    End Sub
+    Private Sub TextBox2_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox2.KeyPress
+        If Asc(e.KeyChar) <> 8 Then
+            If Asc(e.KeyChar) < 48 Or Asc(e.KeyChar) > 57 Then
+                e.Handled = True
+            End If
+        End If
     End Sub
 
     Private Sub SumarEnteros_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+        GroupBox1.Left = Me.Width / 2 - (GroupBox1.Width / 2)
+        Label2.Left = Me.Width / 2 - (Label2.Width / 2)
+        Label5.Left = Me.Width / 2 - (Label5.Width / 2)
     End Sub
 End Class
