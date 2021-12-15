b8c9c00816f376b145cbc3619915a64f763b8d86
PedroArias1800 <86125486+PedroArias1800@users.noreply.github.com>
2021-12-15 16:31:27 -0500

    Revert "DividirEnteros"
    This reverts commit a17435cfddca7205c489b7ef5530bdd53b1fe2d7, reversing
    changes made to 2cacb5843f6e5359ee169118c565d0f22f8fa08e.

--- MiniProyectoDs8/MiniProyectoDs8/GetListByNamevb.vb
+++ MiniProyectoDs8/MiniProyectoDs8/GetListByNamevb.vb
@@ -1,59 +1,21 @@
 ﻿Imports Newtonsoft.Json
-Imports System.Web.Script.Serialization
 
-Imports System.Xml
 Public Class GetListByNamevb
     Dim Servicios As New WebServiceSoap.SOAPDemoSoapClient
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim name As String
-        Dim Data() As String
-        name = txtNombre.Text
-
-        Dim xmlR = XmlReader.Create("https://www.crcind.com/csp/samples/SOAP.Demo.cls?soap_method=GetListByName&name=" & name)
-        Dim al As ArrayList = New ArrayList()
-
-
-
-        Do While xmlR.Read()
-
-
-            If xmlR.NodeType = XmlNodeType.Element AndAlso xmlR.Name = "Name" Then
-                al.Add(xmlR.ReadElementContentAsString & vbCrLf)
-
-            End If
-
-
-
-
-        Loop
-
-
-
-
-
-
-        If al IsNot Nothing Then
-
-            For Each item As String In al
-
-                ListBox1.Items.Add(item)
-
-            Next
-
-        End If
-
-    End Sub
-
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        Dim cadena As String
+        Dim texto As String
         Dim datas() As String
-        cadena = txtNombre.Text
-
-        Dim JsonData As String = ListBox1.Items.Add(JsonConvert.SerializeObject(Servicios.GetListByName(cadena), formatting:=Newtonsoft.Json.Formatting.Indented))
-        datas = Split(JsonData, ".")
-        Dim alldata As JObject = JObject.Parse(JsonData)
+        Dim lo As Long
+        Dim da As Date
+        Dim texto2 As String
+        texto = txtNombre.Text
+        ListBox1.Items.Add(Servicios.GetListByName(texto))
+        Dim JsonData As String = JsonConvert.SerializeObject(Servicios.GetListByName(texto))
+        datas = Split(JsonData, "-")
         MsgBox(JsonData)
     End Sub
+
+
 
 End Class
--- MiniProyectoDs8/MiniProyectoDs8/LookupCity.vb
+++ MiniProyectoDs8/MiniProyectoDs8/LookupCity.vb
@@ -1,16 +1,3 @@
-﻿Imports Newtonsoft.Json
+﻿Public Class __LookupCity
 
-Public Class __LookupCity
-    Dim Servicios As New WebServiceSoap.SOAPDemoSoapClient
-    Private Sub btnBuscar_Click(sender As Object, e As EventArgs) Handles btnBuscar.Click
-        Dim zip As String
-        Dim data(), d1(), d2() As String
-        zip = txtZipCode.Text
-        Dim JsonData As String = JsonConvert.SerializeObject(Servicios.LookupCity(zip))
-        data = Split(JsonData, ",")
-        d1 = Split(data(1), ":")
-        d2 = Split(data(2), ":")
-        txtEstado.Text = d2(1)
-        txtCiudad.Text = d1(1)
-    End Sub
 End Class
